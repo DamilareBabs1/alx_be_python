bd06fc4b4533a8fe9b0354ffac3e51fd12eeeb0c
DamilareBabs1 <Fashinababatunde200@gmail.com>
2025-08-03 19:00:49 +0100

    Commit bank account variables

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+class BankAccount:
+    def __init__ (self, initial_balance=0):
+        self.__account_balance = initial_balance 
+
+    def deposit (self, amount):
+        if amount > 0:
+            self.__account_balance += amount 
+
+    def withdraw (self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount 
+
+    def display_balance (self):
+        print (f"Current Balance: ${self.__account:.2f}")
+
+    def get_balance(self):
+        return self.__account_balance
